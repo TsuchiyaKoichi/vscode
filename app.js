dafcceef1d4599e7f7d06078bf2d1b1486768186
TsuchiyaKoichi <lival1116@gmail.com>
2016-04-12 09:34:22 +0900

    server sample

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require( 'http' );
+var express = require( 'express' );
+
+var app = express();
+var server = http.createServer( app );
+
+
+app.use( express.static( __dirname ) );
+
+app.get( '/', function ( request, response ) {
+  response.redirect( '../index.html' );
+});
+
+server.listen( 3000 );
